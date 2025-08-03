054c5df417a86455aef922770b79428893ca10d3
Tonny Varghese <ca.tonnyvarghese@gmail.com>
2025-08-03 18:53:40 +0000

    Now are we running v2 in server side?

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,5 +1,5 @@
 
-import * as admin from 'firebase-admin';
+import { initializeApp, getApps, App, cert } from 'firebase-admin/app';
 import 'dotenv/config';
 
 // This will be automatically populated by the Firebase environment
@@ -7,18 +7,18 @@ const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
   ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
   : undefined;
 
-let app: admin.app.App;
+let app: App;
 
 // Initialize Firebase Admin SDK
-if (!admin.apps.length) {
+if (!getApps().length) {
   if (!serviceAccount) {
     throw new Error('Firebase Admin SDK service account is not defined. Make sure the FIREBASE_SERVICE_ACCOUNT environment variable is set.');
   }
-  app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  app = initializeApp({
+    credential: cert(serviceAccount),
   });
 } else {
-  app = admin.app();
+  app = getApps()[0];
 }
 
 export const getAdminApp = () => app;
