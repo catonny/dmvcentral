11c6dd82352b479cae04889a1f187f2901d13e8f
Tonny Varghese <ca.tonnyvarghese@gmail.com>
2025-08-03 20:24:15 +0000

    Is the current code working server side ?

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,11 +1,10 @@
-
 import React from "react";
 import { AppLayoutClient } from "@/components/app-layout-client";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-gradient-to-br from-gray-900 to-gray-800 min-h-screen">
+    <body className="bg-background text-foreground">
       <AppLayoutClient>{children}</AppLayoutClient>
-    </div>
+    </body>
   );
 }
