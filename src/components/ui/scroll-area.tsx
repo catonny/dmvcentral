67d862b5732dca73291649cc52a9972b677c75e5
Tonny Varghese <ca.tonnyvarghese@gmail.com>
2025-08-05 15:45:47 +0000

    ok the user is finding it difficult to locate the vertical slidebar, so

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -32,9 +32,9 @@ const ScrollBar = React.forwardRef<
     ref={ref}
     orientation={orientation}
     className={cn(
-      "flex touch-none select-none transition-colors",
+      "flex touch-none select-none transition-all duration-200 ease-out",
       orientation === "vertical" &&
-        "h-full w-2.5 border-l border-l-transparent p-[1px]",
+        "h-full w-2.5 border-l border-l-transparent p-[1px] hover:w-3",
       orientation === "horizontal" &&
         "h-2.5 flex-col border-t border-t-transparent p-[1px]",
       className
